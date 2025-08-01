33e908d1d34eb0ee01489d30513a0f1e46a5d5df
Mirviriam <theraptor_man@hotmail.com>
2025-08-01 01:25:03 -0500

    autotracker2 & tracking_channel have start/stop methodology setup for allowing 1 instance per hosting person

--- AutoTracker2.rb
+++ AutoTracker2.rb
@@ -3,6 +3,8 @@ require 'dotenv/load' # Load environment variables from .env file
 require 'discordrb'
 require_relative 'tracking_channel'
 
+# TODO: extract these constants to a config file
+# TODO: restructure so command_name first, then verb in word order
 # .freeze makes the string immutable, a good practice for constants
 Bot_Prefix = '!'.freeze
 START_VERB = "Start".freeze
@@ -14,7 +16,7 @@ MyEventChannel = 'General'.freeze
 START_REPLY = 'Where does one ever truly begin?'.freeze
 
 server_id = ENV['DISCORD_SERVER_TEST_ID']
-
+tracking_config = {}
 
 bot = Discordrb::Bot.new(
   name: ENV['DISCORD_BOT_NAME'],
@@ -32,6 +34,10 @@ bot.ready do |event|
     # Here we check if the bot is already on the server with the ID MyServerID
     if server.any? { |b| b == server_id }
       puts "Bot is already on server!"
+      bot.server(server_id).channels.each do |channel|
+        puts "Channel: #{channel.name} (ID: #{channel.id}) -- #{channel.text? ? "Text Channel" : "Voice Channel"}"
+        # puts channel.inspect
+      end
     else
       puts "Bot is not on server: #{server[1].name}"
       puts "Click this invite URL to add it:  #{bot.invite_url}"
@@ -40,31 +46,53 @@ bot.ready do |event|
 end
 
 # Bot test receiving messages - !
-bot.message(content: Bot_Prefix) do |event|
-  event.respond START_REPLY
-end
+# bot.message(content: Bot_Prefix) do |event|
+#   unless event.message.content.length < 100
+#     event.respond "Input too long! Keep it under 100 characters."
+#     next
+#   end
+#   author = event.author
+#   voice_channel = bot.server(server_id).member(author.id).voice_channel
+#   puts "Found channel: #{voice_channel.name} (ID: #{voice_channel.id})"
+
+#   voice_channel.send('Hello, this is a message!')
+
+#   event.respond START_REPLY
+# end
 
 # !Start-Tracking command
 bot.message(content: START_CMD) do |event|
   author = event.author
   puts "Message Author: #{author.name}"
-  voice_channel = bot.server(server_id).member(author.id).voice_channel
 
-  # TODO:  Add discord profile versus server profile handler for FC Name
-  event.respond "Certainly Fleet Commander - Starting tracking of fleet members!"
+  voice_channel = bot.server(server_id).member(author.id).voice_channel
 
   tracking_channel = Tracking_Channel.new(
     voice_channel.id,
     voice_channel.name,
+    voice_channel,
     server_id,
     author.id
   )
 
-  puts "Voice Channel: #{tracking_channel.name}"
+  # After we have this channel info stashed in host id, we should use as single source of truth
+  tracking_config[tracking_channel.hoster_id] = tracking_channel
+
+  # TODO:  Add discord profile versus server profile handler for FC Name
+  event.respond "Certainly Fleet Commander - Starting tracking of fleet members!"
+  puts "Voice Channel: #{tracking_config[author.id].id} - #{tracking_config[author.id].name}"
+
+  tracking_config[author.id].channel.send("Fleet Commander has started tracking fleet members!")
+  tracking_config[author.id].channel.send("Please rejoin voice channel to be tracked.")
 end
 
+# !Stop-Tracking command
 bot.message(content: STOP_CMD) do |event|
+  author = event.author
   event.respond 'Certainly Fleet Commander - Stopping tracking of fleet members!'
+  tracking_config[author.id].set_end_time
+  tracking_config[author.id].channel.send("Fleet Commander has stopped tracking fleet members!")
+  puts tracking_config[author.id].host_stopped
 end
 
 
--- tracking_channel.rb
+++ tracking_channel.rb
@@ -1,16 +1,13 @@
 class Tracking_Channel
   # @return [Discorb::Snowflake] The ID of the channel
-  attr_reader :id
-  attr_reader :name
-  attr_reader :server_id
-  attr_reader :hoster_id
-  attr_reader :host_started
-  attr_reader :host_stopped
+  attr_reader :id, :name, :channel, :server_id, :hoster_id
+  attr_reader :host_started, :host_stopped
 
   # Initializes a new Channel instance
-  def initialize(id, name, server_id, hoster_id, host_started = DateTime.now)
+  def initialize(id, name, channel, server_id, hoster_id, host_started = DateTime.now)
     @id = id
     @name = name
+    @channel = channel
     @server_id = server_id
     @hoster_id = hoster_id
     @host_started = host_started
@@ -21,8 +18,8 @@ class Tracking_Channel
     return false unless other.respond_to?(:id)
     @id == other.id
   end
-end
 
   def set_end_time(host_stopped = DateTime.now)
     @host_stopped = host_stopped
   end
+end
