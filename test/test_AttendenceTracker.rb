4bd13b2affb11273ec2ad0b4e8387f5a4a939317
Mirviriam <theraptor_man@hotmail.com>
2025-08-20 09:34:56 -0500

    adding testing

---
+++ test/test_AttendenceTracker.rb
@@ -0,0 +1,48 @@
+require "minitest/autorun"
+require_relative "../AttendenceTracker"
+
+class TestAttendanceTracker < Minitest::Test
+  def setup
+    @tracker = AttendanceTracker.new
+    @user_id = 123
+  end
+
+  def test_initial_total_time_is_zero
+    assert_equal 0, @tracker.total_time(@user_id)
+  end
+
+  def test_user_joined_sets_joined_at
+    @tracker.user_joined(@user_id)
+    refute_nil @tracker.instance_variable_get(:@attendance)[@user_id][:joined_at]
+  end
+
+  def test_user_left_adds_time
+    @tracker.user_joined(@user_id)
+    sleep 1
+    @tracker.user_left(@user_id)
+    assert_operator @tracker.total_time(@user_id), :>=, 1
+    assert_nil @tracker.instance_variable_get(:@attendance)[@user_id][:joined_at]
+  end
+
+  def test_multiple_sessions_accumulate_time
+    @tracker.user_joined(@user_id)
+    sleep 1
+    @tracker.user_left(@user_id)
+    first_total = @tracker.total_time(@user_id)
+    @tracker.user_joined(@user_id)
+    sleep 1
+    @tracker.user_left(@user_id)
+    second_total = @tracker.total_time(@user_id)
+    assert_operator second_total, :>, first_total
+  end
+
+  def test_all_times_returns_hash
+    @tracker.user_joined(@user_id)
+    sleep 1
+    @tracker.user_left(@user_id)
+    times = @tracker.all_times
+    assert_kind_of Hash, times
+    assert times.key?(@user_id)
+    assert_operator times[@user_id], :>=, 1
+  end
+end
