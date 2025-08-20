a7163708dd83ffd4b72db4a754a19d8fb51816de
Mirviriam <theraptor_man@hotmail.com>
2025-08-20 09:09:04 -0500

    adding AttandenceTracker class and calls from autotracker3 to it

---
+++ AttendenceTracker.rb
@@ -0,0 +1,26 @@
+class AttendanceTracker
+  def initialize
+    # { user_id => { joined_at: Time, total_seconds: Integer } }
+    @attendance = Hash.new { |h, k| h[k] = { joined_at: nil, total_seconds: 0 } }
+  end
+
+  def user_joined(user_id)
+    @attendance[user_id][:joined_at] = Time.now
+  end
+
+  def user_left(user_id)
+    joined_at = @attendance[user_id][:joined_at]
+    if joined_at
+      @attendance[user_id][:total_seconds] += (Time.now - joined_at).to_i
+      @attendance[user_id][:joined_at] = nil
+    end
+  end
+
+  def total_time(user_id)
+    @attendance[user_id][:total_seconds]
+  end
+
+  def all_times
+    @attendance.transform_values { |v| v[:total_seconds] }
+  end
+end
